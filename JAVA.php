b6ff9602781ca4578481e81b7ebeaee40d39cc29
DonAmebasPodrido <mattiasaneivacuba@gmail.com>
2025-11-11 11:54:20 -0400

    crear JAVA.php

---
+++ JAVA.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>    <h1> JAVA </h1>
+
+<h3>SINTAXIS DE JAVA</h3>
+<p></p>
+<h3>ESTRUCTURAS REPETITIVAS</h3>
+<p></p>
+<h3>POO (ATRIBUTOS Y METODOS )</h3>
+<p></p>
+<h3>HERENCIA</h3>
+<p></p>
+ <h3>CRUD</h3>
+ <p></p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</center>
+ 
+</body>
+</html>
