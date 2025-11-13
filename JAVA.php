3812bc0eb8f3de6c40e1fbc937a9045595806ecd
DonAmebasPodrido <escobarcordovamaria@pedropoveda.edu.bo>
2025-11-13 14:24:25 -0400

    sintaxis JAVA

--- JAVA.php
+++ JAVA.php
@@ -6,19 +6,75 @@
     <title>Document</title>
 </head>
 <body>
-    <center>    <h1> JAVA </h1>
+    <center>   <big> <h1> JAVA </h1><big> </center>
 
-<h3>SINTAXIS DE JAVA</h3>
+<h2>SINTAXIS DE JAVA</h2>
+
+<p>  <h3> 🔹 Estructura general</h3>
+public class Main {<br>
+    public static void main(String[] args) {<br>
+        System.out.println("Hola, profe!");<br>
+    }<br>
+}
+
+ 
+
+<h3> 🔹 caracteristicas - JAVA</h3> 
+* Todo programa tiene una clase y un método main.<br>
+* Las llaves {} delimitan bloques de código.<br>
+* Las líneas terminan con ;.<br>
+ 
+<h3> 🔹 Tipos de datos</h3>
+Tipo	Descripción	Ejemplo<br>
+int	Entero	int edad = 20;<br>
+double	Decimal	double precio = 10.5;<br>
+char	Carácter	char letra = 'A';<br>
+boolean	Verdadero/Falso	boolean activo = true;<br>
+String	Texto	String nombre = "Ana";<br>
+
+<h3> 🔹 Estructuras de control </h3>
+if (x > 0) { ... } else { ... }<br>
+for (int i = 0; i < 5; i++) { ... }<br>
+while (condición) { ... }<br>
+
+
+* Controlan el flujo del programa.<br>
+
+<h3>🔹 Métodos</h3>
+void saludar() {<br>
+    System.out.println("Hola!");<br>
+}<br>
+
+* Bloques de código reutilizables.<br>
+* Se pueden recibir parámetros y devolver valores.<br>
+ 
+<h3> 🔹 Clases y Objetos</h3>
+
+class Persona {<br>
+    String nombre;<br>
+    void saludar() {<br>
+        System.out.println("Hola " + nombre);<br>
+    }
+}
+<br>
+
+* Java es orientado a objetos.<br>
+* Las clases definen estructuras y los objetos son instancias<br> </p>
+
+
+ 
+
 <p></p>
-<h3>ESTRUCTURAS REPETITIVAS</h3>
-<p></p>
+
 <h3>POO (ATRIBUTOS Y METODOS )</h3>
 <p></p>
+
 <h3>HERENCIA</h3>
 <p></p>
+
  <h3>CRUD</h3>
  <p></p>
- este es el cambio para esta imilla de los rulos traviesos
+ 
 
 
 
@@ -48,8 +104,7 @@
 
 
 
-
-</center>
+ 
  
 </body>
 </html>
