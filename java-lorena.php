5f8d6dcb4af7f83007b6c991bbc8daacffb5922c
gonzaleslorenita <gonzalesvalencialorena@pedropoveda.edu.bo>
2025-11-25 12:19:01 -0400

    acabado

--- java-lorena.php
+++ java-lorena.php
@@ -4,8 +4,50 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <center><h1>JAVA LORENA</h1></center>
+    <h2>SINTAXIS DE JAVA</h2><br>
+
+    <p>public class hola {<br>
+    public static void main(String[] args) {<br>
+        System.out.println("");<br>
+    }<br>
+}<br>
+</p>
+    <h2>ESTRUCTURAS REPETITIVAS</h2><br>
+
+    <P>for (int i = 0; i < 10; i++) {<br>
+    System.out.println(i);<br>
+}<br>
+    while (numero > 0) {<br>
+    numero--;<br>
+}<br>
+    do {<br>
+    opcion = leerOpcion();<br>
+} while (opcion != 0);<br>
+<h2>POO (ATRIBUTOS Y METODOS )</h2><br>
+
+<p><h3>ATRIBUTOS</h3><br>
+Son variables dentro de una clase. Representan el estado de un objeto.<br>
+class Persona {<br>
+    String nombre;<br>
+    int edad;<br>
+}<br>
+<h3>METODOS</h3><br>
+Son las acciones o comportamientos de los objetos.<br>
+void saludar() {<br>
+    System.out.println("Hola");<br>
+}<br>
+<h2>HERENCIA</h2><br>
+<p>La herencia es uno de los pilares fundamentales de la Programación Orientada a Objetos en Java.
+Consiste en que una clase (llamada clase hija, subclase o derivada) puede heredar atributos y métodos de otra clase (llamada clase padre, superclase o base).
+
+Esto permite reutilizar código, organizar mejor los programas y representar relaciones del mundo real.</p><br>
+
+
+
+</p>
 </head>
 <body>
-    <h1>JAVA DOC</h1>
+    
 </body>
 </html>
